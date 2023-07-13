9146cb1097a458755e89370b235dd4d4ac4bdec9
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2023-07-12 22:05:30 -0400

    some additions for validation

--- migrations/20230712182035-create-recipes.js
+++ migrations/20230712182035-create-recipes.js
@@ -12,25 +12,55 @@ module.exports = {
       title: {
         type: Sequelize.STRING,
         allowNull: false,
+        validate: {
+          len: {
+            args: [3],
+            msg: "Title must be at least 3 characters long",
+          },
+        },
       },
       description: {
         type: Sequelize.TEXT,
+        validate: {
+          len: {
+            args: [0, 500],
+            msg: "Description must be at most 500 characters long",
+          },
+        },
       },
       ingredients: {
         type: Sequelize.TEXT,
         allowNull: false,
+        validate: {
+          len: {
+            args: [500, 1000],
+            msg: "Description must be at most 1000 characters long",
+          },
+        },
       },
       instructions: {
         type: Sequelize.TEXT,
         allowNull: false,
+        validate: {
+          len: {
+            args: [0, 5000],
+            msg: "Description must be at most 5000 characters long",
+          },
+        },
       },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        validate: {
+          isDate: true,
+        },
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
+        validate: {
+          isDate: true,
+        },
       },
     });
   },
